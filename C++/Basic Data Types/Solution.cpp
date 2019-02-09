07654237b380fa6ff2ac54701c4879892dee9d6f
Pranav Chaudhary <pranavchaudhary2310@gmail.com>
2019-02-09 16:05:37 +0530

    Add Basic Data Types

---
+++ C++/Basic Data Types/Solution.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#define endl '\n'
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    scanf("%d %ld %c %f %lf", &a, &b, &c, &d, &e);
+    printf("%d\n%ld\n%c\n%f\n%lf\n", a, b, c, d, e);
+}
