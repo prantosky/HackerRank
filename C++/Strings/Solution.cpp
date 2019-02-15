5cbdf2d0be427a386ffb34ba2a360ebd68d64a2b
Pranav Chaudhary <pranavchaudhary2310@gmail.com>
2019-02-15 09:56:28 +0530

    Solve Strings

---
+++ C++/Strings/Solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    string a, b;
+    cin >> a >> b;
+    cout << a.size() << " " << b.size() << '\n';
+    cout << a + b << '\n';
+    char temp = a[0];
+    a[0] = b[0];
+    b[0] = temp;
+    cout << a << " " << b << '\n';
+    return 0;
+}
